b67adc65907a3c84ed750e56b0dc04f5faef31f8
Raghavendra <72355127+raghuworld-git@users.noreply.github.com>
2022-04-05 17:03:16 +0530

    Configured to set localtimezone if not present
    Configured to set localtimezone if not present in localstorage.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Subscription } from 'rxjs';
 import { LoaderService } from './core/services/loader.service';
+import { TimeZoneService } from './core/services/timezone-service.service';
 
 @Component({
   selector: 'app-root',
@@ -8,12 +9,13 @@ import { LoaderService } from './core/services/loader.service';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit, OnDestroy {
-  constructor(private loader: LoaderService) { }
+  constructor(private loader: LoaderService,private tzService:TimeZoneService) { }
   isLoading = false;
 
   private subscribe: Subscription;  
 
-  ngOnInit() {        
+  ngOnInit() {    
+    this.tzService.getBrowserTimeZone();  
     this.subscribe = this.loader.loading$.subscribe((value) => {
       Promise.resolve(null).then(() => this.isLoading = value);
     })
