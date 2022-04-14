c1d4d92be7f6e01a6c0c358f53ee75232bc628c1
Raghav <raghuworld1992@outlook.com>
2022-04-14 13:53:29 +0530

    changing datetime of launch update created by as per timezone change

--- src/app/features/launches/components/launch-updates/launch-updates.component.ts
+++ src/app/features/launches/components/launch-updates/launch-updates.component.ts
@@ -1,19 +1,32 @@
-import { ChangeDetectionStrategy, Component, Input, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input, OnDestroy, OnInit } from '@angular/core';
+import { Observable, Subscription } from 'rxjs';
+import { TimeZoneService } from 'src/app/core/services/timezone-service.service';
 import { ILaunchUpdatesModel } from 'src/app/shared/models/launch/launchUpdates.model';
 
 @Component({
   selector: 'app-launch-updates',
   templateUrl: './launch-updates.component.html',
   styleUrls: ['./launch-updates.component.scss'],
-  changeDetection:ChangeDetectionStrategy.OnPush
+  changeDetection:ChangeDetectionStrategy.Default
 })
-export class LaunchUpdatesComponent implements OnInit {
+export class LaunchUpdatesComponent implements OnInit,OnDestroy {
 
-  constructor() { }
+  constructor(private tzService:TimeZoneService) { }
 
   @Input() update:ILaunchUpdatesModel;
+  @Input() $timezone:Observable<string>;
+
+  createdOnDate:string='';
+  private subscription:Subscription;
 
   ngOnInit(): void {
+   this.subscription= this.$timezone.subscribe((tzone)=>{
+       this.createdOnDate = this.tzService.getChangeDateTimeByTimeZone(this.update.created_on,tzone);
+    })
+  }
+
+  ngOnDestroy(): void {
+    this.subscription?.unsubscribe();
   }
 
 }
